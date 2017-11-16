c9bccc2341e77197afff51c52a032e409c480326
Lucas Silveira <lucas.fernandes.silveira@gmail.com>
2017-11-16 17:30:49 -0200

    Base structure app with Eslint, Absolute Paths and Reactotron working fine

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/* Core */
+import React from 'react';
+
+/* Presentational */
+import { View } from 'react-native';
+
+// import styles from './styles';
+const App = () => (
+  <View />
+);
+
+export default App;
